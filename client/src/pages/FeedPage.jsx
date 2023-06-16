6dbede1bd4ac821a6066b7618b15a4294a8b0617
Kim Daehyeok <133863274+KimTerMan@users.noreply.github.com>
2023-06-16 15:06:00 +0900

    0616NewFeed

--- client/src/pages/FeedPage.jsx
+++ client/src/pages/FeedPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Post from '../components/Feed/Post'
 
 const FeedPage = () => {
   return (
-    <div>FeedPage</div>
+    <div>
+      <Post />
+    </div>
   )
 }
 

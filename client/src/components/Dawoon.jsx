00449d5cccd13007a4e60c4da4067fbe0615ec75
orthh <witchicken2@gmail.com>
2023-06-16 09:22:06 +0900

    폴더정리

--- client/src/components/Dawoon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dawoon = () => {
-  return (
-    <div>Dawoon</div>
-  )
-}
-
-export default Dawoon
--- client/src/components/Dayo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dayo = () => {
-  return (
-    <div>Dayo123123123</div>
-  )
-}
-
-export default Dayo

d83cc520b940420b7563ee813d13b33e7d057257
orthh <witchicken2@gmail.com>
2023-06-20 17:53:48 +0900

    design : 메인페이지에 발자국지도 추가

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import "../assets/css/Main.css"
+import React from "react";
+import "../assets/css/Main.css";
 
-import FootMap from '../components/Main/FootMap'
-import Carousel from '../components/Main/Carousel'
+import FootMap from "../components/Main/FootMap";
+import Carousel from "../components/Main/Carousel";
+import FootTracer from "../components/FootTrace/FootTracer";
 
 const MainPage = () => {
   return (
-
-    
     <div>
-      <div width="100px">
+      <div width="100px"></div>
+      <div className="main_foottracer">
+        <FootTracer />
       </div>
-      <FootMap/>
-      <Carousel/>
+      <Carousel />
     </div>
+  );
+};
 
-  
-  )
-}
-
-export default MainPage
+export default MainPage;
